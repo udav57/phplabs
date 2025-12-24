262d6a544701bf33482992ab26825aeaa06f45e3
udav57 <105563508+udav57@users.noreply.github.com>
2025-12-24 12:27:41 +0300

    Create config-sample.php

---
+++ 8/config-sample.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'login_db');
+define('DB_CHARSET', 'utf8mb4');
