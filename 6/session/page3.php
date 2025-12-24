398529a0a5fc8813ffd7c20b4792f0c3930ed80d
udav57 <105563508+udav57@users.noreply.github.com>
2025-12-24 12:31:29 +0300

    Update page3.php

--- 6/session/page3.php
+++ 6/session/page3.php
@@ -1,12 +1,11 @@
 <?php
+declare(strict_types= 1);
 
-declare(strict_types=1);
-
-ini_set("session.use_only_cookies", "0");
-ini_set("session.use_trans_sid", "1");
-
+@ini_set("session.use_only_cookies", "0");
+@ini_set("session.use_trans_sid", "1");
 
 session_start();
+
 
 include('savepage.inc.php');
 ?>
